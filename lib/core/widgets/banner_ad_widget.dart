9f9c95ab74008e7666ba5a9b6bbc29d0c1edf7a6
mohamed-yaseen <01221316865m@gmail.com>
2025-06-11 17:35:02 +0300

    Upload To Google Play

--- lib/core/widgets/banner_ad_widget.dart
+++ lib/core/widgets/banner_ad_widget.dart
@@ -18,7 +18,7 @@ class _BannerAdWidgetState extends State<BannerAdWidget> {
     super.initState();
 
     _bannerAd = BannerAd(
-      adUnitId: 'ca-app-pub-3940256099942544/6300978111',
+      adUnitId: 'ca-app-pub-3001005463674932/6896100270',
       size: AdSize.banner,
       request: const AdRequest(),
       listener: BannerAdListener(
