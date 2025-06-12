16fffe7c5a69322af75152360382ebbb6d57410e
mohamed-yaseen <01221316865m@gmail.com>
2025-06-12 15:50:36 +0300

    Upload To Google Play 6

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
         targetSdk = flutter.targetSdkVersion
-        versionCode = 2
-        versionName = "1.4.9"
+        versionCode = 3
+        versionName = "1.5.0"
     }
 
     signingConfigs {
