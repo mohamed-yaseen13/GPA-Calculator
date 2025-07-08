569c5bfdfe305c1ec9a72026647322efe86e61e5
mohamed-yaseen <01221316865m@gmail.com>
2025-07-08 09:22:52 +0300

    finish the app 2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -53,7 +53,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
         targetSdk = flutter.targetSdkVersion
-        versionCode = 6
+        versionCode = 8
         versionName = pubVersionName
 
     }
