28e629b8f8065d7f403e3e062751b8ab82b7c666
mohamed-yaseen <01221316865m@gmail.com>
2025-07-28 10:57:00 +0300

    Upload To Google Play Update version 16

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -53,7 +53,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
         targetSdk = flutter.targetSdkVersion
-        versionCode = 15
+        versionCode = 16
         versionName = pubVersionName
 
     }
