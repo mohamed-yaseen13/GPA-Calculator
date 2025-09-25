52f9a0ae2166117ecb158422d7e9b54cc3b99e81
mohamed-yaseen <01221316865m@gmail.com>
2025-09-25 17:28:48 +0300

    Upgrade build gradle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -51,7 +51,7 @@ android {
         applicationId = "com.engmo.gpacalculator"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = 17
         versionName = pubVersionName
